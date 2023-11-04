94a35251626ebd5e7bb2e42c9d3d63bca9e0690a
huidong.yin <huidong.yin247203@gmail.com>
2023-11-04 15:13:37 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -e
 
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 deploy_branch=gh-pages # 推送的分支
-user_name=FireHH
-user_email=m18388461281@163.com
+user_name=huidongyin
+user_email=huidong.yin247203@gmail.com
 
 # 生成静态文件
 npm run build
@@ -14,14 +14,13 @@ npm run build
 # 进入生成的文件夹
 cd $dist_path
 
-#发布到自定义域名
-echo 'www.javaessay.cn' > CNAME
+
 if [ -z "$GITHUB_TOKEN" ]; then  # -z 字符串 长度为0则为true；$GITHUB_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
   msg='deploy'
-  githubUrl=git@github.com:FireHH/Java-essay.git
+  githubUrl=git@github.com:huidongyin/huidongyin.github.io.git
 else
   msg='Action workflow deploy'
-  githubUrl=https://FireHH:${GITHUB_TOKEN}@github.com/FireHH/Java-essay.git
+  githubUrl=https://huidongyin:${GITHUB_TOKEN}@github.com/huidongyin/huidongyin.github.io.git
 fi
 git init
 git config user.name $user_name
