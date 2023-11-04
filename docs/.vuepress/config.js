2c7dc94e9696315f97fcfdfa6acb585bff260df9
huidong.yin <huidong.yin247203@gmail.com>
2023-11-04 10:52:44 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,42 +4,42 @@ const dayjs = require("dayjs");
 
 module.exports = {
   base: "/",
-  title: 'Java essay',
+  title: 'Huidong Blogs',
   // theme: require.resolve('../../theme/vdoing'),
   theme: 'vdoing',
-  description: '后端技术网站,专注Java学习与总结，面试。Java基础，Spring,SpringBoot,SpringCloud,SpringCloudAlibaba,数据库设计,中间件,性能优化,开源技术等技术文章。',
+  description: '花花世界迷人眼，没有实力别赛脸。',
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: "Java essay",
-      description: '后端技术网站,专注Java学习与总结，面试。Java基础，Spring,SpringBoot,SpringCloud,SpringCloudAlibaba,数据库设计,中间件,性能优化,开源技术等技术文章。',
+      title: "Huidong Blogs",
+      description: '花花世界迷人眼，没有实力别赛脸。',
     }
   },
   markdown: {
     lineNumbers: true, // 代码块显示行号
-    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    extractHeaders: ['h1','h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
   },
   themeConfig: {
-    logo: '/img/logo.png',
-    lastUpdated: '最后更新时间',
+    logo: '/img/code.gif',
+    lastUpdated: 'lastUpdateTime',
     smoothScroll: true, //页面滚动
     sidebarDepth:2, //侧边栏自动显示当前激活页面中标题的链接，嵌套在页面本身的链接下。默认深度是1，提取h2标题；0禁用标题链接；最大值为2，同时提取h2和h3。
     nav:require("./nav.js"),
     // repo: 'FireHH/Java-essay',
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     editLinks: false, // 启用编辑
-    editLinkText: '编辑',
+    editLinkText: 'edit',
     // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
     sidebar: 'structuring', 
     author: {
-      name: 'Mr.Fire', // 必需
-      link: 'https://github.com/FireHH', // 可选的
+      name: 'huidong.yin', // 必需
+      link: 'https://huidongyin.github.io', // 可选的
     },
     // 博主信息 (显示在首页侧边栏)
     blogger: {
-      avatar: '/img/huge.jpg',
-      name: 'Mr.Fire',
-      slogan: '后端程序员一枚',
+      avatar: '/img/code.gif',
+      name: 'huidong.yin',
+      slogan: '我不是大佬',
     },
     categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
     // bodyBgImg: [
@@ -56,17 +56,17 @@ module.exports = {
         {
           iconClass: 'icon-youjian',
           title: '发邮件',
-          link: 'mailto:firelumf@qq.com',
+          link: 'mailto:huidong.yin247203@gamil.com',
         },
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/FireHH/',
+          link: 'https://github.com/huidongyin/',
         },
         {
           iconClass: 'icon-gitee',
           title: 'Gitee',
-          link: 'https://gitee.com/firegitspace/',
+          link: 'https://gitee.com/yin_huidong/',
         },
         {
           iconClass: 'icon-erji',
@@ -78,16 +78,16 @@ module.exports = {
 
     // 页脚信息
     footer: {
-      createYear: 2021, // 博客创建年份
+      createYear: 2023, // 博客创建年份
       copyrightInfo:
-          'Mr.Fire | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+          'huidong.yin | <a href="https://huidongyin.github.io" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
     extendFrontmatter: {
       author: {
-        name: 'Mr.Fire',
-        link: 'https://gitee.com/firegitspace/'
+        name: 'huidong.yin',
+        link: 'https://huidongyin.github.io'
       }
     },
 
@@ -132,15 +132,15 @@ module.exports = {
       // ],
     
       // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-      // 'fulltext-search',
+       'fulltext-search',
     
       [
         'one-click-copy', // 代码块复制按钮
         {
           copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-          copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-          duration: 1000, // prompt message display time.
-          showInMobile: false, // whether to display on the mobile side, default: false.
+          copyMessage: 'copy success', // default is 'Copy successfully and then paste it for use.'
+          duration: 10000, // prompt message display time.
+          showInMobile: true, // whether to display on the mobile side, default: false.
         },
       ],
         
--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,175 +1,154 @@
-module.exports=[
-    {
-        text: '首页',
-        link: '/',
-    },
+module.exports = [
     {
         text: 'Java基础',
         items: [
             {
-                text: 'Java基础',
+                text: 'JavaSE',
+                link: '/pages/c41b1f/',
+            }, {
+                text: '并发编程',
                 link: '/pages/c41b1f/',
             },
             {
-                text: 'Java进阶',
+                text: 'JavaWeb',
                 link: '/pages/c41b1f/',
             },
             {
-                text: '设计模式',
+                text: '版本新特性',
+                link: '/pages/2be000/',
+            }
+        ],
+    },
+    {
+        text: 'Spring全家桶',
+        items: [
+            {
+                text: 'Spring',
+                link: '/pages/c41b1f/',
+            },
+            {
+                text: 'SpringMvc',
+                link: '/pages/c41b1f/',
+            },
+            {
+                text: 'Mybatis',
                 link: '/pages/2be000/',
             },
             {
-                text: 'Java你不知道的小事',
-                link: '/pages/java-small-story.md',
-            },
-        ],
-    },
-    {
-        text: 'Spring',
-        items: [
-            {
-                text: 'Spring初识',
-                link: '/pages/113e8b/',
+                text: 'SpringBoot',
+                link: '/pages/2be000/',
             },
             {
-                text: 'Spring进阶',
-                link: '/pages/152a03/',
-            },
-        ],
-    },
-    {
-        text: 'SpringBoot',
-        items: [
-            {
-                text: 'SpringBoot基础',
-                link: '/pages/171a40/',
-            },
-            {
-                text: 'SpringBoot进阶',
-                link: '/pages/ebbfd3/',
-            },
-        ],
-    },
-    {
-        text: '分布式微服务',
-        items: [
-            {
-                text: '什么是微服务',
-                link: '/pages/2f8402/',
-            },
-            {
-                text: 'SpringCloud全家桶',
-                link: '/pages/59a2b4/',
+                text: 'SpringCloud',
+                link: '/pages/2be000/',
             },
             {
                 text: 'Dubbo',
-                link: '/pages/dubbo-alibaba.md',
+                link: '/pages/2be000/',
             },
             {
-                text: 'SpringCloud Alibaba',
-                link: '/pages/spring-cloud-alibaba.md',
+                text: 'Zookeeper',
+                link: '/pages/2be000/',
+            },
+            {
+                text: 'Netty',
+                link: '/pages/2be000/',
+            },
+            {
+                text: 'Tomcat',
+                link: '/pages/2be000/',
             }
         ],
     },
     {
-        text: '中间件+云原生',
+        text: "消息队列",
         items: [
             {
-                text: 'Zookeeper',
-                link: '/pages/09fbd0/',
-            },
-            {
-                text: 'Nginx',
-                link: '',
-            },
-            {
-                text: 'RabbitMQ',
-                link: '/pages/08d72f/',
+                text: 'Kafka',
+                link: '/pages/c8412896-0c96-3cef-8d32-94dcb1852140/'
             },
             {
                 text: 'RocketMQ',
-                link: '/pages/25443e/',
+                link: '/pages/rocketmq'
             },
             {
-                text: 'Docker入门到精通',
-                link: '/pages/1e77b5/'
+                text: 'RabbitMQ',
+                link: '/pages/rabbitmq'
             }
         ]
     },
     {
-        text: '性能分析优化',
+        text: "数据库",
         items: [
             {
-                text: '性能分析工具',
-                link: '/pages/8f7142/'
+                text: 'MySQL',
+                link: '/pages/elasticsearch'
             },
             {
-                text: '数据库性能优化',
-                link: '/pages/bdbd0d/'
+                text: 'Redis',
+                link: '/pages/elasticsearch'
+            }
+        ]
+    }, {
+        text: "微服务架构",
+        items: [
+            {
+                text: '架构演进',
+                link: '/pages/elasticsearch'
             },
             {
-                text: '性能优化',
-                link: '/pages/4b3850/'
+                text: '分布式事务',
+                link: '/pages/elasticsearch'
             },
-            
+            {
+                text: '设计模式',
+                link: '/pages/elasticsearch'
+            },
+            {
+                text: '领域驱动设计',
+                link: '/pages/elasticsearch'
+            }
         ]
     },
     {
-        text: '面试',
+        text: "搜索引擎",
         items: [
             {
-                text: 'Java基础面试必问',
-                link: '/pages/3d1eb9/',
-            },
+                text: 'ElasticSearch',
+                link: '/pages/elasticsearch'
+            }
+        ]
+    }, {
+        text: "JVM",
+        link: '/',
+    }, {
+        text: "云计算",
+        items: [
             {
-                text :'JavaWeb面试必问',
-                link :'/pages/98558b/'
-            },
-            {
-                text :'Java框架面试必问',
-                link :''
-            },
-            {
-                text :'数据库面试必问',
-                link :''
-            },
-            {
-                text :'中间件面试必问',
-                link :''
-            },
-            {
-                text :'分布式微服务面试必问',
-                link :'/pages/06bd99'
-            },
-            {
-                text :'Linux面试必问',
-                link :''
-            },
-            {
-                text: '计算机网络面试必问',
-                link: '/pages/cc54d6/',
-            },
-            {
-                text :'开放性问题面试问必问',
-                link :''
-            },
-        ],
+                text: 'Docker',
+                link: '/pages/elasticsearch'
+            }
+        ]
     },
     {
-        text: '关于我',
+        text: '日常开发',
         items: [
             {
-                text: '简介',
-                link: '/pages/3312d0/',
-            },
+                text:'Mac使用日常',
+                link: '/pages/1c1021ad-9e46-3426-be54-98f2d02a1fa5/',
+            }
+        ]
+    },
+    {
+        text: 'AboutMe',
+        items: [
             {
-                text: '联系我',
-                link: '/pages/a30957/',
-            },
+                text: '个人简历',
+                link: '/pages/aboutme/',
+            }, {
+                text: '项目经验',
+                link: '/pages/a2a9d5e6-b580-3ee0-b2c2-5b1557852fd5/',
+            }
         ],
     }
-    // {
-    //     text: 'Gitee',
-    //     link: 'https://gitee.com/firegitspace/java-essay.git',
-    // }
 ]
